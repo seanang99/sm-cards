1cf9ca09d9cd2c0c47963120e3c7a0db72706b11
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-21 16:00:51 +0000

    Update App.js with React Router and remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+const Home = lazy(() => import('./components/pages/Home'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<div>Page not found</div>} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
