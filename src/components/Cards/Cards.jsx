cdf04a35ba0e6ed66e57a74d2b7bfa1d6703dfb5
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-21 16:00:10 +0000

    Create Cards component

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
+const Cards = (props) => {
+    const { title, subtitle, content, test } = props;
+
+    return (
+        <Card>
+            {test ?
+                <>
+                    <Card.Header>{subtitle}</Card.Header>
+                    <Card.Body>
+                        <Form>
+                            <Form.Group controlId='Form.ControlTitle'>
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type='text' className='form-input form-input___text' />
+                            </Form.Group>
+
+                            <Form.Group controlId='Form.ControlVerse'>
+                                <Form.Label>Verse</Form.Label>
+                                <Form.Control as='textarea' rows={3} className='form-input form-input___textarea' />
+                            </Form.Group>
+                            <Button className="form-button" variant="primary" as='input' type="submit" value="Check"/>
+                            <Button className="form-button" variant="light" as='input' type="reset" value="Clear"/>
+                        </Form>
+                    </Card.Body>
+                </> :
+                <>
+                    <Card.Header>{title}</Card.Header>
+                    <Card.Body>
+                        <Card.Subtitle>{subtitle}</Card.Subtitle>
+                        <Card.Text>{content}</Card.Text>
+                    </Card.Body>
+                </>
+            }
+
+        </Card>
+    )
+}
+
+export default Cards;
