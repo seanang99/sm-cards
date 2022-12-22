2e9d0a99598c208d1b39a40c707762c34daeb4c5
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-22 16:17:33 +0000

    Create Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Uni Navigators Computer Training Programme Fall 2022</p>
+    </div>
+  );
+};
+
+export default Footer;
