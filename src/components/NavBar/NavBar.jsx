6e23c3bed633c2d768b8bf9eafa20c2270fe57c9
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-22 16:17:51 +0000

    Create NavBar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+
+import { MOCK_MEMORY_VERSE_PACK } from "../../mocks/MockVerses";
+
+import "./style.scss";
+
+const NavBar = (props) => {
+  const { testMode, setTestMode, setPack } = props;
+
+  return (
+    <>
+      <Navbar fixed="top" bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">SM</Navbar.Brand>
+          <div className="nav-bar__right">
+            <Form.Check
+              type="switch"
+              id="test-mode-switch"
+              label="Test Mode"
+              checked={testMode}
+              onChange={() => setTestMode(!testMode)}
+            />
+            <Form.Select
+              className="nav-bar__select"
+              aria-label="Select Memory Verse Pack"
+              onChange={(e) => setPack(e.target.value)}
+            >
+              <option>Select Pack</option>
+              {MOCK_MEMORY_VERSE_PACK.map((pack) => {
+                return (
+                  <option key={pack} id={pack} value={pack}>
+                    {pack}
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
