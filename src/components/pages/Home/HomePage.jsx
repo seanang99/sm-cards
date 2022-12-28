8557aa2e9683551dd7430c36fc5d48dc3ebcd63a
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-28 13:17:35 +0000

    Refactor HomePage component, add styling and add useEffect functions for API calls

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,46 +1,82 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
+import { React, useState, useEffect } from "react";
 import Cards from "../../Cards";
+import NavBar from "../../NavBar/NavBar";
+import axios from "axios";
 
-import { MOCK_MEMORY_VERSE_PACK, MOCK_VERSES } from '../../../mocks/MockVerses';
+import "./style.scss";
 
 const HomePage = () => {
-    return (
-        <div className='page-layout'>
-            <header>
-                <Navbar bg='light' expand='lg'>
-                    <Container>
-                        <Navbar.Brand href='/'>SM</Navbar.Brand>
-                        <div className='nav-bar__right'>
-                            <Form>
-                                <Form.Check type='switch' id='test-mode-switch' label='Test Mode' />
-                                <Form.Select className='nav-bar__select' aria-label='Select Memory Verse Pack'>
-                                    <option>Select Pack</option>
-                                    {MOCK_MEMORY_VERSE_PACK.map((pack) => {
-                                        return (
-                                            <option key={pack} id={pack} value={pack}>{pack}</option>
-                                        );
-                                    })}
-                                </Form.Select>
-                            </Form>
-                        </div>
-                    </Container>
-                </Navbar>
-            </header>
+  const [testMode, setTestMode] = useState(false);
+  const [packs, setPacks] = useState([]);
+  const [allVerses, setAllVerses] = useState([]);
+  const [mvPack, setMvPack] = useState("");
+  const [verses, setVerses] = useState([]);
 
-            <div className='page-body'>
-                {/* Cards */}
-                {MOCK_VERSES.map((v) => {
-                    return (
-                        <Cards key={`${v.id} + ${v.memory_pack}`} test={false} title={v.title} subtitle={v.reference} content={v.verse} />
-                    )
-                })}
-            </div>
-        </div>
+  // API calls to get Memory Packs and all verses on first load
+  useEffect(() => {
+    const getMemoryPack = () => {
+      axios.get(`${process.env.REACT_APP_API_URL}/packs`).then((res) => {
+        const data = res.data;
+        setPacks(data);
+      });
+    };
 
-    );
+    const getAllVerses = () => {
+      axios.get(`${process.env.REACT_APP_API_URL}/verses`).then((res) => {
+        const data = res.data;
+        setAllVerses(data.verses);
+      });
+    };
+
+    getAllVerses();
+    getMemoryPack();
+  }, []);
+
+  // Filter verses to display when mvPack is selected
+  useEffect(() => {
+    const filterMemoryVerses = (memory_pack) => {
+        if (memory_pack === "TMS - 60"){
+            return allVerses.filter((v) => v.memory_pack.includes("TMS 60"));
+        }
+        return allVerses.filter((v) => v.memory_pack === memory_pack);
+      };
+
+    setVerses(filterMemoryVerses(mvPack));
+  }, [allVerses, mvPack]);
+
+  return (
+    <div className="page-layout">
+      <header>
+        <NavBar
+          testMode={testMode}
+          setTestMode={setTestMode}
+          pack={packs}
+          setPack={setMvPack}
+        />
+      </header>
+      <div className="page-body">
+        {verses.length ? (
+          <>
+            {verses.map((v) => {
+              return (
+                <Cards
+                  key={`${v.id} + ${v.memory_pack}`}
+                  test={testMode}
+                  title={v.title}
+                  subtitle={v.reference}
+                  content={v.verse}
+                />
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <p>Please select a Memory Pack</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
