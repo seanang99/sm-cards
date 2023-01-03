f042398d9890c9f8c6f3f42e67710998c223b329
Sean <54630058+seanang99@users.noreply.github.com>
2023-01-03 05:27:57 +0000

    Add findLCS function

--- src/utils/checkSM.js
+++ src/utils/checkSM.js
@@ -4,13 +4,15 @@
  * @returns score, correct_ans
  */
 export const checkTitleAndVerse = (res) => {
-    const {title, verse, userTitle, userVerse} = res;
+    const { title, verse, userTitle, userVerse } = res;
 
-    if (typeof (userTitle) === 'string' && typeof (userVerse) === 'string' && typeof(title) === 'string' && typeof(verse) === 'string') {
+    if (typeof (userTitle) === 'string' && typeof (userVerse) === 'string' && typeof (title) === 'string' && typeof (verse) === 'string') {
+        /* eslint-disable */
         const uTitle = userTitle.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
         const cTitle = title.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
         const cVerse = verse.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ").split(' ');
         const uVerse = userVerse.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ").split(' ');
+        /* eslint-enable */
 
         let score = 0;
 
@@ -18,14 +20,110 @@ export const checkTitleAndVerse = (res) => {
             score += 1;
         }
 
-        for (let i = 0; i < cVerse.length; i++) {
-            if (uVerse[i] !== cVerse[i]) {
-                score++;
+        let cIndex = 0, uIndex = 0;
+
+        while (uIndex < uVerse.length - 1) {
+
+            if (score >= 5) {
+                break;
             }
+
+            if (uVerse[uIndex] !== cVerse[cIndex]) {
+
+                /**
+                 * Potentially Missing word -> Find the word in verse  
+                 * 1) If indexOf word is greater than cIndex (which it should be), it means that there are extra words
+                 * 2) Add to score 
+                 * */
+                const _uIndex = uVerse.indexOf(cVerse[cIndex]);
+
+                if (_uIndex === -1) {
+                    // Missing words
+                    uVerse.splice(uIndex, uIndex - 1, cVerse[cIndex]);
+                    score++;
+                } else if (_uIndex !== cIndex) {
+                    score = score + (_uIndex - cIndex);
+                    uIndex = _uIndex;
+                }
+                console.log({ _uIndex, uVerse: uVerse[uIndex], cVerse: cVerse[cIndex], arr: uVerse, uIndex, score });
+            }
+
+            cIndex++;
+            uIndex++;
         }
-        
+
         const correct = score === 0 ? true : false
         return { score: Math.min(score, 5), correct };
     }
+}
 
+export function findLCS(str1, str2) {
+    let longest = 0, longestSubstring = "", index = -1;
+
+    for (let i = 0; i < str1.length; i++) {
+        for (let j = i + 1; j <= str1.length; j++) {
+            const subString = str1.substring(i, j);
+            if (str2.includes(subString) && subString.length > longest) {
+                longest = subString.length;
+                longestSubstring = subString;
+                index = i;
+            }
+        }
+    }
+    return { longestSubstring, index };
+}
+
+/**
+ * Function to find the longest common subsequence of 2 arrays
+ * @param {*} A 
+ * @param {*} B 
+ * @param {*} n 
+ * @param {*} m 
+ */
+export function findLongestCommonSubsequence(A, B, m, n) {
+    //Create new 2D array
+    var dp = new Array(m + 1).fill().map(() => Array(n + 1).fill(0));
+
+    // Store length of the longest common substring
+    var result = 0;
+    var commonWords = [];
+
+    // Updating the dp[][] table in Bottom Up approach
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i === 0 || j === 0)
+                dp[i][j] = 0;
+            else if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                result = Math.max(result, dp[i][j]);
+                commonWords.push(A[i - 1]);
+                // console.log({ word: A[i - 1], commonWords, i, j })
+            }
+            else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    // Find all subStrings
+    let i, j, startIndex = 0, endIndex = 0, subStrings = [];
+    for (i = 0; i < commonWords.length; i++) {
+        let tempArr = [];
+        startIndex = A.indexOf(commonWords[i]);
+        endIndex = startIndex;
+
+        for (j = i; j < commonWords.length; j++) {
+            // If the next few elements are in the same order, add them to tempArr
+            if (A[endIndex] === commonWords[j]) {
+                tempArr.push(A[endIndex]);
+                endIndex++;
+            } else {
+                break;
+            }
+        }
+        i = j - 1; // fast forward i to last element of confirmed substring
+        subStrings.push({ subString: tempArr, startIndex, endIndex });
+    }
+
+    return { result, subStrings };
 }
--- src/utils/checkSM.test.js
+++ src/utils/checkSM.test.js
@@ -1,20 +1,23 @@
-import { checkTitleAndVerse } from './checkSM';
+import { checkTitleAndVerse, findLCS, findLongestCommonSubsequence } from './checkSM';
 
-describe('checkSM function', () => {
+describe('checkSM', () => {
 
-    const correctTitle = "Assurance of Salvation"
-    const correctVerse = "And this is the testimony: God has given us eternal life, and this life is in his Son. Whoever has the Son has life; whoever does not have the Son of God does not have life."
+    const defaultTitle = "Assurance of Salvation"
+    const defaultVerse = "And this is the testimony: God has given us eternal life, and this life is in his Son. Whoever has the Son has life; whoever does not have the Son of God does not have life."
 
     const correct_userTitle = "Assurance of Salvation"
     const correct_userVerse = "And this is the testimony: God has given us eternal life, and this life is in his Son. Whoever has the Son has life; whoever does not have the Son of God does not have life."
 
+    const partially_wrong_userVerse_0 = "this is the testimony: God has given us eternal life, and this life is in his Son. Whoever has the Son has life; whoever does not have the Son of God does not have life."
+    const partially_wrong_userVerse_1 = "this and this is the testimony: God has given us eternal life, and this life is in his Son. Whoever has the Son has life; whoever does not have the Son of God does not have life."
+
     const wrong_userTitle = "Assurance of Forgiveness"
     const wrong_userVerse = "Therefore, if anyone is in Christ, he is a new creation; the old has gone, the new has come!"
 
-    test('default correct title', () => {
+    test('default correct title and verse', () => {
         const data = {
-            title: correctTitle,
-            verse: correctVerse,
+            title: defaultTitle,
+            verse: defaultVerse,
             userTitle: correct_userTitle,
             userVerse: correct_userVerse
         }
@@ -27,8 +30,8 @@ describe('checkSM function', () => {
 
     test('incorrect title but correct verse should result in a score: 1', () => {
         const data = {
-            title: correctTitle,
-            verse: correctVerse,
+            title: defaultTitle,
+            verse: defaultVerse,
             userTitle: wrong_userTitle,
             userVerse: correct_userVerse
         }
@@ -39,16 +42,85 @@ describe('checkSM function', () => {
         expect(res).toEqual(ex_res);
     })
 
+    test('correct title but incorrect verse should result in a score: 5 if there are more than 5 mistakes', () => {
+        const data = {
+            title: defaultTitle,
+            verse: defaultVerse,
+            userTitle: correct_userTitle,
+            userVerse: wrong_userVerse,
+        }
+
+        const res = checkTitleAndVerse(data)
+        const ex_res = { score: 5, correct: false }
+
+        expect(res).toEqual(ex_res);
+    })
+
+    test('correct title but partially incorrect verse with 1 missing word should result in a score: 1', () => {
+        const data = {
+            title: defaultTitle,
+            verse: defaultVerse,
+            userTitle: correct_userTitle,
+            userVerse: partially_wrong_userVerse_0,
+        }
+
+        const res = checkTitleAndVerse(data)
+        const ex_res = { score: 1, correct: false }
+
+        expect(res).toEqual(ex_res);
+    })
+
+    test('correct title but partially incorrect verse with 1 extra word should result in a score: 1', () => {
+        const data = {
+            title: defaultTitle,
+            verse: defaultVerse,
+            userTitle: correct_userTitle,
+            userVerse: partially_wrong_userVerse_1,
+        }
+
+        const res = checkTitleAndVerse(data)
+        const ex_res = { score: 1, correct: false }
+
+        expect(res).toEqual(ex_res);
+    })
+
+
     test('completely incorrect title and verse should result in a score of 5', () => {
         const data = {
-            title: correctTitle,
-            verse: correctVerse,
+            title: defaultTitle,
+            verse: defaultVerse,
             userTitle: wrong_userTitle,
             userVerse: wrong_userVerse
         }
-        
+
         const res = checkTitleAndVerse(data)
         const ex_res = { score: 5, correct: false }
         expect(res).toEqual(ex_res);
+    })
+})
+
+describe('FindLongestCommonSubsequence', () => {
+
+    // Driver Code
+    let A = ["And", "this", "is", "the", "testimony", "God", "has", "given", "eternal", "life"];
+    let B = ["And", "this", "a", "is", "the", "testimony", "given"];
+    let C = ["what", "is", "this", "testimony"]
+
+    const defaultVerse = "And this is the testimony: God has given us eternal life, and this life is in his Son. Whoever has the Son has life; whoever does not have the Son of God does not have life."
+    const alternateVerse = "hello world, God has given us eternal life, and this life is in his Son. whoever does not have the Son of God does not have life."
+
+
+    test('default', () => {
+        const results = { result: 3, subStrings: [{ subString: ['And', 'this', 'is', 'the', 'testimony'], startIndex: 0, endIndex: 5 }, { subString: ['given'], startIndex: 7, endIndex: 8 }] }
+        expect(findLongestCommonSubsequence(A, B, A.length, B.length)).toStrictEqual(results);
+    })
+
+    test('B & C', () => {
+        const results = { result: 1, subStrings: [{ subString: ["this"], startIndex: 1, endIndex: 2 }, { subString: ["is"], startIndex: 2, endIndex: 3 }, { subString: ["testimony"], startIndex: 5, endIndex: 6 }] }
+        expect(findLongestCommonSubsequence(B, C, B.length, C.length)).toStrictEqual(results);
+    })
+
+    test('longestSubString', () => {
+        expect(findLCS(alternateVerse, defaultVerse)).toEqual({ longestSubstring: " God has given us eternal life, and this life is in his Son. ", index: 12 });
     })
 })
