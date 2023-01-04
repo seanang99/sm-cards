45f8a29287fbc504c36b273fbfa6e7e1a240d0f0
Sean <54630058+seanang99@users.noreply.github.com>
2023-01-04 12:57:57 +0000

    Update card to include validation and amend styles to be mobile friendly

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
+import { checkTitleAndVerse } from "../../utils/checkSM";
 
 import "./style.scss";
 
 const Cards = (props) => {
   const { title, subtitle, content, test } = props;
+
+  const [userTitle, setUserTitle] = useState("");
+  const [userVerse, setUserVerse] = useState("");
+
+  const [validateTitle, setValidateTitle] = useState(false);
+  const [validateVerse, setValidateVerse] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [isCorrect, setIsCorrect] = useState();
+
+  const markTitleAndVerse = () => {
+    const data = {
+      title, verse: content, userTitle, userVerse,
+    }
+    const res = checkTitleAndVerse(data);
+    console.log({ res });
+    setIsCorrect(res);
+  }
+
+  const handleOnSubmit = () => {
+    // Validate Title and Verse inputs
+    setValidateTitle(!userTitle);
+    setValidateVerse(!userVerse);
+
+    if (userTitle && userVerse) {
+      markTitleAndVerse();
+      setSubmitted(true);
+    }
+  }
+
+  const handleFeedback = (isCorrect, title, content) => {
+    const { correctTitle, correctVerse, correct } = isCorrect;
+    let feedback = "";
+
+    if (!correctTitle && !correctVerse) {
+      feedback = "Incorrect Title and Verse!";
+    } else if (!correctTitle) {
+      feedback = "Incorrect Title!"
+    } else if (!correctVerse) {
+      feedback = "Incorrect Verse!"
+    } else {
+      feedback = "Correct Answer! Well done!"
+    }
+
+    const divClassName = `feedback__${correct ? "correct" : "wrong"}`;
+
+    return (
+      <>
+        <p className={`feedback ${divClassName}`}>{feedback}</p>
+        {!correctTitle ? <div className={`correction-title ${divClassName}`}>{title}</div> : <></>}
+        {!correctVerse ? <div className={`correction-verse ${divClassName}`}>{content}</div> : <></>}
+      </>
+    )
+  }
 
   return (
     <Card>
@@ -14,38 +68,53 @@ const Cards = (props) => {
         <>
           <Card.Header>{subtitle}</Card.Header>
           <Card.Body>
-            <Form>
-              <Form.Group controlId="Form.ControlTitle">
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  type="text"
-                  className="form-input form-input___text"
-                />
-              </Form.Group>
+            <Form.Group controlId="Form.ControlTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                className={`form-input ${validateTitle ? "form-input-invalid" : ""}`}
+                value={userTitle}
+                onChange={(e) => {
+                  setValidateTitle(!e.target.value);
+                  setUserTitle(e.target.value);
+                }}
+              />
+              <p className={`form-control-validator${validateTitle ? "" : "__hidden"}`}>
+                Title cannot be blank
+              </p>
+            </Form.Group>
 
-              <Form.Group controlId="Form.ControlVerse">
-                <Form.Label>Verse</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  className="form-input form-input___textarea"
-                />
-              </Form.Group>
-              <Button
-                className="form-button"
-                variant="primary"
-                as="input"
-                type="submit"
-                value="Check"
+            <Form.Group controlId="Form.ControlVerse">
+              <Form.Label>Verse</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                className={`form-input ${validateVerse ? "form-input-invalid" : ""}`}
+                value={userVerse}
+                onChange={(e) => {
+                  setValidateVerse(!e.target.value);
+                  setUserVerse(e.target.value);
+                }}
               />
-              <Button
-                className="form-button"
-                variant="light"
-                as="input"
-                type="reset"
-                value="Clear"
-              />
-            </Form>
+              <p className={`form-control-validator${validateVerse ? "" : "__hidden"}`}>
+                Verse cannot be blank
+              </p>
+            </Form.Group>
+            <Button
+              className="form-button"
+              variant="primary"
+              as="input"
+              value="Check"
+              onClick={() => handleOnSubmit()}
+            />
+
+            {submitted ?
+              <>
+                <footer>
+                  {handleFeedback(isCorrect, title, content)}
+                </footer>
+              </> : <></>
+            }
           </Card.Body>
         </>
       ) : (
