167e9b1d3d2cabd4ceb718483757cb9854f50369
Sean <54630058+seanang99@users.noreply.github.com>
2023-01-04 12:57:28 +0000

    create checkSM function

---
+++ src/utils/checkSM.js
@@ -0,0 +1,21 @@
+/**
+ * Function checks the correctness of the verse
+ * @param {*} props 
+ */
+export const checkTitleAndVerse = (props) => {
+    const { title, verse, userTitle, userVerse } = props;
+
+    if (typeof (userTitle) === 'string' && typeof (userVerse) === 'string' && typeof (verse) === 'string' && typeof (title) === 'string') {
+
+        const uTitle = userTitle.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
+        const cTitle = title.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
+        const uVerse = userVerse.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
+        const cVerse = verse.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
+
+        let correctTitle = uTitle !== cTitle ? false : true;
+        let correctVerse = uVerse !== cVerse ? false : true;
+        let correct = correctTitle && correctVerse;
+
+        return { correct, correctTitle, correctVerse }
+    }
+}
