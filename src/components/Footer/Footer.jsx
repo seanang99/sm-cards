37d6934bbdc36e2bae4cf4904f24cd05c64a8d7f
Sean Ang <e0345669@u.nus.edu>
2024-02-20 00:15:56 +0800

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { ReactComponent as GithubLogo } from "../../resources/icons/github-mark-white.svg";
 import "./style.scss";
 
-/**
+/** 
  * Renders Footer component by default
  * @returns Footer component
  */
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Uni Navigators Computer Training Programme Fall 2022</p>
+      <div>
+        <p className="description">Uni Navigators Computer Training Programme Fall 2022</p>
+        <a href="https://github.com/seanang99/sm-cards">
+          <GithubLogo className="githubLogo" />
+        </a>
+      </div>
     </div>
   );
 };
